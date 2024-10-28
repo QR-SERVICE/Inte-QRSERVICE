2e18ec81c10a2626d1372e99e7d70073021839a5
AlonsoVA <128510867+AlonsoVA@users.noreply.github.com>
2024-10-27 23:49:11 -0600

    Las rutas ahora son de manera estaticos, las cuales son manjebles de una manera mas sencilla, pero al mismo tiempo el acomodo es distino, tambien estoy haciendo pruebas con el menu , para lo que es el diseño, tambien en configuraciones hice algunas pruebas. Lo importate cree una carpeta Public donde se mueven todos los diseños, JS, JSON. Se creo una carpeta IMG en la cual estan todas las imagenes que vamos a usar, lo que es el logo etc. Y cree APP en donde estan todos los archvios, pamntallas el index etc.

---
+++ app/Public/jMenu.js
@@ -0,0 +1,204 @@
+// Boton para abrir la sidebar
+const menu = document.getElementById('menu-desplegable');
+const sidebar = document.getElementById('sidebar');
+
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('menu-toggle');
+})
+
+// cambiar estado de seleccionado en el menu
+const menuButtons = document.querySelectorAll('.menu-boton');
+
+    menuButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            menuButtons.forEach(btn => btn.classList.remove('a-selected'));
+            button.classList.add('a-selected');
+        });
+    });
+
+// Boton para abrir y cerrar las opciones del menu en modo responsivo
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+// Boton para mostrar lo que llevan de orden 
+const carrito = document.getElementById("carrito");
+const ver = document.getElementById("carr")
+const NoVer = document.getElementById("cerrar_carr")
+
+ver.addEventListener("click", () => {
+    carrito.style.display = 'block'; 
+    carrito.style.zIndex = 1; 
+    setTimeout(() => {
+        carrito.classList.add("visible"); 
+    }, 10); 
+});
+
+NoVer.addEventListener("click", () => {
+    carrito.classList.remove("visible"); 
+    carrito.addEventListener('transitionend', () => {
+        carrito.style.display = 'none'; 
+    }, { once: true }); 
+});
+// Boton para mostrar las opciones de notificaciones
+
+const notificaciones = document.getElementById("notificaciones");
+const cerrar_notis = document.getElementById("cerrar-notis");
+const abrir_notis = document.getElementById("abrir-notis");
+
+abrir_notis.addEventListener("click", () => {
+    notificaciones.style.display = 'block'; 
+    notificaciones.style.zIndex = 1; 
+    setTimeout(() => {
+        notificaciones.classList.add("visible"); 
+    }, 10); 
+})
+
+cerrar_notis.addEventListener("click", () => {
+    notificaciones.classList.remove("visible");
+})
+
+
+// Boton de traduccion
+const langButtons = document.querySelectorAll("[data-language]");
+langButtons.forEach((button) =>{
+    button.addEventListener("click", () =>{
+        const texttochange = document.querySelectorAll("[data-seccion]");
+        console.log(texttochange);
+        fetch(`/languages/${button.dataset.language}.json`)
+        .then(res => res.json())
+        .then(data => {
+            texttochange.forEach((el) => {
+                const seccion = el.dataset.seccion;
+                const valor = el.dataset.valor;
+
+                el.textContent = data[seccion][valor];
+                
+            })
+        })
+    })
+})
+
+// Boton para cambiar de categoria de productos
+const opcionButtons = document.querySelectorAll("[data-categoria]");
+opcionButtons.forEach((button) =>{
+    button.addEventListener("click",()=>{
+        const texTochange = document.querySelectorAll("[data-section]");
+        console.log(texTochange);
+        fetch(`../Menu/Productos.js/${button.dataset.categoria}.json`)
+        .then(res => res.json())
+        .then(data =>{
+            texTochange.forEach((op) => {
+                const section = op.dataset.section;
+                const value = op.dataset.value;
+
+                op.textContent= data[section][value];
+            })
+        })
+    })
+})
+
+const sumProducts = document.querySelectorAll(".S-boton");
+const nombresP = document.querySelectorAll(".nombreP");
+const preciosP = document.querySelectorAll(".costo");
+const productOrde = document.getElementById("productos_orden");
+const TOTAL = document.getElementById("TOTAL");
+let totalSum = 0;
+
+sumProducts.forEach((sumProduct, index) => {
+    sumProduct.addEventListener("click", () => {
+        const name = nombresP[index].textContent;
+        const precio = parseFloat(preciosP[index].textContent);
+        const agregarOrden = document.createElement("td");
+        const parrafoName = document.createElement("p")
+        parrafoName.className = "name";
+        const parrafoprice = document.createElement("p")
+        parrafoprice.className = "price";
+        const nombre = document.createTextNode(name);
+        parrafoName.appendChild(nombre)
+        const price = document.createTextNode(" "+ precio.toFixed(2) + "$");
+        parrafoprice.appendChild(price)
+        const restProduct = document.createElement("button");
+        agregarOrden.className = ("datos-producto")
+        restProduct.className = "bi bi-trash";
+        restProduct.id =("bote-basura");
+        restProduct.addEventListener("click", () => {
+            agregarOrden.remove();
+            restProduct.remove();
+            totalSum -= precio;
+            TOTAL.textContent = totalSum.toFixed(2) + "$";
+
+        });
+        agregarOrden.appendChild(parrafoName);
+        agregarOrden.appendChild(parrafoprice);
+        productOrde.appendChild(agregarOrden);
+        productOrde.appendChild(restProduct);
+
+        totalSum += precio;
+        TOTAL.textContent = totalSum.toFixed(2) + "$";
+                
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const botonEn = document.getElementById("enviar-t");
+    if (botonEn) {
+        botonEn.addEventListener("click", () => {
+            const productosguardador = []; 
+            const productosName = document.querySelectorAll(".name");
+            const productosPrice = document.querySelectorAll(".price");
+            productosName.forEach((nameElement, index) => {
+                const name = nameElement.textContent;
+                const price = productosPrice[index].textContent.replace("$", "").trim();
+                productosguardador.push({
+                    name: name,
+                    price: price
+                });
+            });
+            const total = document.getElementById("TOTAL").textContent;
+            localStorage.setItem("total", total);
+            localStorage.setItem("lst", JSON.stringify(productosguardador));
+            console.log("Lista de productos guardada:", JSON.parse(localStorage.getItem("lst")));
+            console.log("Total guardado:", localStorage.getItem("total"));
+        });
+    } else {
+        console.error("Error al guardar los datos");
+    }
+});
+
+// cerrar si se hace click fuera de notificaciones
+const notificacionesM = document.querySelector('#notificaciones');
+
+notificacionesM.addEventListener("click", cerrarNotificacion);
+
+function cerrarNotificacion() {
+    notificacionesM.classList.toggle("visible");
+}
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    const menuNotificacion = target === notificacionesM || notificacionesM.contains(target);
+
+    if (!menuNotificacion) {
+        notificacionesM.classList.remove("visible");
+    }
+});
+
+// cerrar si se hace click fuera de la barra de menu
+document.addEventListener('click', (event) => {
+    const ClickDentroSidebar = sidebar.contains(event.target);
+    const ClickDentroMenu = menu.contains(event.target);
+
+    if (!ClickDentroSidebar && !ClickDentroMenu) {
+        sidebar.classList.remove('menu-toggle');
+    }
+});
---
+++ app/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+
+// Obtiene el nombre del archivo actual
+const __filename = fileURLToPath(import.meta.url);
+// Obtiene el directorio actual
+const __dirname = path.dirname(__filename);
+
+const server = express();
+const PORT = 3500; // Cambia el puerto según sea necesario
+
+// Configuración de directorios estáticos
+server.use(express.static(path.join(__dirname, 'Public')));
+server.use(express.static(path.join(__dirname, 'Img')));
+server.use(cors());
+
+// Rutas
+server.get("/", (req, res) => res.sendFile(path.join(__dirname, 'Configuracion', 'Productos_confi.html')))
+server.get("/confi", (req, res) => res.sendFile(path.join(__dirname, 'Configuracion', 'Configuraciones.html')))
+server.get("/Menu", (req, res) => res.sendFile(path.join(__dirname, 'Menu', 'Menu-Orden.HTML')))
+server.get("/Me", (req, res) => res.sendFile(path.join(__dirname, 'Menu', 'Menu-copy.HTML')))
+server.get("/Comandas", (req, res) => res.sendFile(path.join(__dirname, 'PantallaComandas', 'index.HTML')))
+server.get("/Pedidos", (req, res) => res.sendFile(path.join(__dirname, 'Pedidos', 'Historial_pedidos.HTML')))
+
+// Inicia el servidor
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+/* JSON Para el funcionamiento de las imagenes.
+
+const ImgJson =[
+    {nombre: "Juan", foto: "https://www.unileverfoodsolutions.com.co/dam/global-ufs/mcos/NOLA/calcmenu/recipes/col-recipies/fruco-tomate-cocineros/HAMBURGUESA%201200x709.png"}
+];
+
+const Pro_img =[
+    {}
+]
+
+app.get("/", (req, res) => res.json(ImgJson[0]));
+app.get("/prueba1", (req, res) => res.json(ImgJson[1]));*/
